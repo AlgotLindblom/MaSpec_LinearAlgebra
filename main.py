80cbb462897596ff86224e8dd658d14f17f3c864
algot <algot.lindblom@gmail.com>
2023-12-07 10:04:46 +0100

    Fixat beräkning av determinant

--- main.py
+++ main.py
@@ -36,15 +36,21 @@ def isSquare(M):
 
 def matriceDet(M):
     '''Takes an NumPy array or ndarray of integers or floats of size 1x1, 2x2 or 3x3 and returns its determinant as a float.'''
+    if not isinstance(M, np.ndarray):
+        print("ERROR: Invalid input. Datatype must be NumPy array.")
+        return
     if not isSquare(M):
         print("ERROR: Invalid input. Input matrix M must be square.")
-        return
-    
-    
+        return 
+    if M.shape == (2, 2):
+        return M[0][0 ] * M[1][1] - M[0][1] * M[1][0]
+    if M.shape == (3, 3):
+        return (M[0][0] * (M[1][1] * M[2][2] - M[1][2] * M[2][1]) - M[0][1] * (M[1][0] * M[2][2] - M[1][2] * M[2][0]) +  M[0][2] * (M[1][0] * M[2][1] - M[1][1] * M[2][0]))
+    print(f"Matrix must be either 2x2 or 3x3. Your matrix has shape {M.shape[0]}x{M.shape[1]}.")
 
 def multiply(A, B):
     if (A.shape[1] != B.shape[0]):
-        print(f"ERROR: Invalid input. Input matrices A and B must match. {A.shape[1]} must equal {B.shape[9]}.")
+        print(f"ERROR: Invalid input. Input matrices A and B must match. {A.shape[1]} must equal {B.shape[0]}.")
         return
     M = np.ndarray(A.shape[0], B.shape[1])
     M = A * B
@@ -52,5 +58,4 @@ def multiply(A, B):
 
 if __name__ == "__main__":
     M = makeArray()
-    print(M)
-    #matriceDet(M)
+    print(matriceDet(M))
